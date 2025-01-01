1e975512efa873b071b75598a21a4d99092e1627
sirUnchained <bbehnam3831@gmail.com>
2025-01-01 14:11:41 +0330

    finished user entity, dto

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,15 +1,35 @@
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import userRoles from '../enums/user.enums';
 
-export class User {
+@Entity({ name: 'users' })
+export class UserEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'string', unique: true })
+  @Column({ type: 'string', unique: true, nullable: false })
   username: string;
 
-  @Column({ type: 'string', unique: true })
+  @Column({ type: 'string', unique: true, nullable: false })
   email: string;
 
-  @Column({ type: 'string', unique: true })
+  @Column({ type: 'string', unique: true, nullable: false })
   phone: string;
+
+  @Column({ type: 'string', nullable: true })
+  password: string;
+
+  @Column({ type: 'enum', enum: userRoles, default: userRoles.user })
+  role: userRoles;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
---
+++ src/users/enums/user.enums.ts
@@ -0,0 +1,6 @@
+enum userRoles {
+  admin = 'admin',
+  user = 'user',
+}
+
+export default userRoles;
