be370d740cd37fd6a5f5108f044688ab4f4fb2f0
sirUnchained <bbehnam3831@gmail.com>
2025-01-03 17:48:53 +0330

    start jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,28 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/users/entities/user.entity';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+JwtStrategy;
+JwtModule;
+PassportModule;
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService): Promise<any> => ({
+        secret: configService.get('JWT_SECRET'),
+        signOption: { expiresIn: '1d' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
